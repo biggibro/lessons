58522ca210fde22331cfb42b7d8a050d2d780734
Андрей <andrej@MacBook-Pro-Andrej.local>
2025-05-13 22:59:19 +0500

    init setup

---
+++ lesson_1/js/script.js
@@ -0,0 +1,10 @@
+console.log('JS подключен!');
+
+// Здесь можно начинать писать ваш JavaScript код
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM полностью загружен');
+
+    // Пример кода
+    const heading = document.querySelector('h1');
+    heading.style.color = 'blue';
+});
